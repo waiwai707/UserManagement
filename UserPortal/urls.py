d4388ee052eab88cbac9f2801e01666fb14fd5a5
Phyo-Hayman-Kyaw <phyohmk.verbena@gmail.com>
2023-12-03 14:09:47 +0630

    add update and delete method

--- UserPortal/urls.py
+++ UserPortal/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('sign_up/', views.signup, name = 'sign_up'),
     path('create/', views.create, name = 'create'),
+    path('update/int:<pk>', views.update, name = 'update'),
+    path('edit/int:<pk>', views.edit, name = 'edit'),
+    path('delete/int:<pk>', views.delete, name = 'delete'),
 ]
