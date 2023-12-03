791d3d35723c9c8935bf0d88f29419462551aef1
Phyo-Hayman-Kyaw <phyohmk.verbena@gmail.com>
2023-12-03 11:51:36 +0630

    add models and css

--- UserPortal/models.py
+++ UserPortal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    dob = models.CharField(max_length=100)
+    email = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=100)
+    status = models.CharField(max_length=100,null=True)
