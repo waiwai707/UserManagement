e136a46f91d5e81907289c20e42df316bc372a64
Phyo-Hayman-Kyaw <phyohmk.verbena@gmail.com>
2023-12-13 00:00:39 +0630

    add login and signup

--- UserPortal/urls.py
+++ UserPortal/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from UserPortal import views
 
 urlpatterns = [
+    path('', views.index, name = 'index'),
     path('home/', views.home, name = 'home'),
-    path('sign_up/', views.signup, name = 'sign_up'),
+    path('log_in/', views.user_login, name = 'login'),
+    path('sign_up/', views.user_signup, name = 'signup'),
+    path('log_out/', views.user_logout, name = 'logout'),
     path('create/', views.create, name = 'create'),
     path('update/int:<pk>', views.update, name = 'update'),
     path('edit/int:<pk>', views.edit, name = 'edit'),
